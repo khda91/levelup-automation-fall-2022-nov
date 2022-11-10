9cb96aa7b7fc2792f2ae313cd73e5117f3296287
Dmitry Khodakovsky <khda91@gmail.com>
2022-11-10 20:55:14 +0300

    subtract double and examples

--- src/main/java/ru/levelp/at/lesson02/git/App.java
+++ src/main/java/ru/levelp/at/lesson02/git/App.java
@@ -16,6 +16,18 @@ public class App {
         System.out.printf("%d - %d = %d%n", 5, 10, calculator.subtract(5, 10));
         System.out.println("==========");
         System.out.println();
+
+        System.out.println("===== Multiply =====");
+        System.out.printf("%d * %d = %d%n", 2, 2, calculator.multiply(2, 2));
+        System.out.printf("%d * %d = %d%n", 5, 10, calculator.multiply(5, 10));
+        System.out.println("==========");
+        System.out.println();
+
+        System.out.println("===== Divide =====");
+        System.out.printf("%.2f / %.2f = %.2f%n", 2, 2, calculator.divide(2D, 2D));
+        System.out.printf("%.2f / %.2f = %.2f%n", 5, 10, calculator.divide(5D, 10D));
+        System.out.println("==========");
+        System.out.println();
     }
 
 }
--- src/main/java/ru/levelp/at/lesson02/git/Calculator.java
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -10,6 +10,10 @@ public class Calculator {
         return a - b;
     }
 
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
     public int multiply(int a, int b) {
         return a * b;
     }
