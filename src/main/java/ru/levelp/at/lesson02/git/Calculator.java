8f40543bba20f8ef2c275830a6800105f0baf40b
Dmitry Khodakovsky <khda91@gmail.com>
2022-11-10 21:17:46 +0300

    added pow mehtod

--- src/main/java/ru/levelp/at/lesson02/git/Calculator.java
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -22,18 +22,30 @@ public class Calculator {
         return a * b;
     }
 
-    public double multiply(double a, double b) {
-        return a * b;
-    }
+    public int pow(int a, int b) {
+        if (b < 0) {
+            throw new IllegalArgumentException("Не умеем считать отрицательные степени!!!");
+        }
 
-    public double divide(double a, double b) {
-        return a / b;
+        if (b == 0) {
+            return 1;
+        }
+
+        int result = 1;
+        for (int i = 1; i <= b; i++) {
+            result *= a;
+        }
+        return result;
     }
 
     public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Делить на 0 нельзя!!!");
         }
+        return a / b;
+    }
+
+    public double divide(double a, double b) {
         return a / b;
     }
 }
