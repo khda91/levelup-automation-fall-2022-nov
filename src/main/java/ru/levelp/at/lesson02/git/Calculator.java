eee71a3dc2131176090bb4b5f4747c15476bb2b1
Dmitry Khodakovsky <khda91@gmail.com>
2022-11-10 20:52:21 +0300

    added multiply double method

--- src/main/java/ru/levelp/at/lesson02/git/Calculator.java
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -14,6 +14,10 @@ public class Calculator {
         return a * b;
     }
 
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
     public double divide(double a, double b) {
         return a / b;
     }
