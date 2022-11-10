890dcc3c76ce874edcf54f2f84398b2aabe3ad3f
Dmitry Khodakovsky <khda91@gmail.com>
2022-11-10 21:42:40 +0300

    delete sum

--- src/main/java/ru/levelp/at/lesson02/git/Calculator.java
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -5,11 +5,6 @@ public class Calculator {
     public int sum(int a, int b) {
         return a + b;
     }
-
-    public double sum(double a, double b) {
-        return a + b;
-    }
-
     public int subtract(int a, int b) {
         return a - b;
     }
